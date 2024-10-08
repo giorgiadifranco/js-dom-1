33717c2908cf19ae4a6895aabb75625913f83ed2
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-08 15:04:29 +0200

    add selezione elementi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,31 @@
 console.log("it works");
+
+
+//1. selezionare gli elementi della DOM
+
+//- fare la variabile buttonEl;
+const buttonEl = document.getElementById('button')
+console.log(buttonEl);
+
+
+//- creare variabile imgEl 
+const imgEl = document.getElementsByTagName('img')
+
+//2. collegare un EventListner al pulsante (in questo caso l'accensione);
+//buttonEl.addEventListener('click', ()=>{imgEl.classList('acceso')})
+
+buttonEl.addEventListener("click", function() {
+    buttonEl.getElementById("click").innerHTML =  `<button>"Spegni"<button> `;
+});
+
+
+/* From W3School
+element.addEventListener("click", function() {
+  document.getElementById("demo").innerHTML = "Hello World";
+});*/
+
+
+
+//Cosa dovrebbe accadere:
+//-il bottone cambia scritta al click;
+//-l'immagine cambia al click
