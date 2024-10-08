a55e373ec04d76ae5e9c57d007edcbecee28b04d
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-08 16:31:30 +0200

    ADD condition and includes

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,19 +11,13 @@ const imgEl = document.getElementById('spento')
 //2. collegare un EventListner al pulsante (in questo caso l'accensione);
 //buttonEl.addEventListener('click', ()=>{imgEl.classList('acceso')})
 
-    buttonEl.addEventListener('click', ()=>{document.getElementById("button").innerHTML = "Spegni";
+//SENZA CONDITION
+
+    /*buttonEl.addEventListener('click', ()=>{document.getElementById("button").innerHTML = "Spegni";
         imgEl.src= './img/yellow_lamp.png';
-    });
+    });*/
 
     
-
-
-
-
-
-
-
-
 
 /* From W3School
 element.addEventListener("click", function() {
@@ -35,3 +29,19 @@ element.addEventListener("click", function() {
 //Cosa dovrebbe accadere:
 //-il bottone cambia scritta al click;
 //-l'immagine cambia al click
+
+//FUNCTION + CONDITION + INCLUDES
+buttonEl.addEventListener('click', function lightLamp (){
+
+
+    if(imgEl.src.includes('white_lamp.png')){
+
+        imgEl.src= './img/yellow_lamp.png';
+        buttonEl.innerHTML= "Spegni";
+
+    }else{
+        imgEl.src= './img/white_lamp';
+        buttonEl.innerHTML= "Accendi"
+    }
+}
+)
