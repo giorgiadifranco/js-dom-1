bed227c0128dc8b862c4b59ed99c0617f25ade26
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-08 15:33:28 +0200

    add innerHTML buttn

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,19 +4,26 @@ console.log("it works");
 //1. selezionare gli elementi della DOM
 
 //- fare la variabile buttonEl;
-const buttonEl = document.getElementById('button')
-console.log(buttonEl);
+const buttonEl = document.getElementById("button")
+//console.log(buttonEl);
+
+    buttonEl.addEventListener('click', ()=>{document.getElementById("button").innerHTML = "I have changed!"});
+
+// HTML button element
+
+// Change label text
+//buttonEl.innerHTML = 'Spegni';
+
+//console.log(buttonEl.innerHTML);
 
 
 //- creare variabile imgEl 
-const imgEl = document.getElementsByTagName('img')
+//const imgEl = document.getElementsByTagName('img')
 
 //2. collegare un EventListner al pulsante (in questo caso l'accensione);
 //buttonEl.addEventListener('click', ()=>{imgEl.classList('acceso')})
 
-buttonEl.addEventListener("click", function() {
-    buttonEl.getElementById("click").innerHTML =  `<button>"Spegni"<button> `;
-});
+
 
 
 /* From W3School
